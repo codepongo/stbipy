19c185313ce5d2cc4496bf17aed2a26472b6b622
unknown <zhuhuotui@gmail.com>
2016-01-11 17:16:22 +0800

    clean temporary files for test

--- tstbi.py
+++ tstbi.py
@@ -1,10 +1,10 @@
 import stbi
 
 im = stbi.open('t.jpg')
-#im.save('t.png')
-#im.save('t.bmp', 'bmp')
-#im.crop(100, 100, 400, 400).save('t-resize.png')
-#im.gray().save('t-gray.png')
+im.save('t.png')
+im.save('t.bmp', 'bmp')
+im.crop(100, 100, 400, 400).save('t-resize.png')
+im.gray().save('t-gray.png')
 d = im.data
 sz = im.size
 byte = 0
@@ -15,12 +15,13 @@ for pixel in xrange(sz[0] * sz[1]):
     byte += 4
 im.data = d
 im.save('t-invert-color.png')
-#print 'input any key to clean the result...'
-#raw_input()
-#import os
-#os.remove('t.png')
-#os.remove('t.bmp')
-#os.remove('t-resize.png')
-#os.remove('t-gray.png')
-#os.remove('t-red.png')
+raw_input('input any key to clean the result...')
+import os
+os.remove('t.png')
+os.remove('t.bmp')
+os.remove('t-resize.png')
+os.remove('t-gray.png')
+os.remove('t-invert-color.png')
+
+
 
